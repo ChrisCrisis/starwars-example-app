ddd4ae5c6f00d02a59c5a4ad1ad32e403eeb1168
Chris Weber <chris.weber@moguru.de>
2023-07-17 17:10:15 +0200

    add basic ship list

--- app/src/main/java/dev/glimmr/starwarssample/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/dev/glimmr/starwarssample/ui/screen/home/HomeScreen.kt
@@ -1,37 +1,114 @@
 package dev.glimmr.starwarssample.ui.screen.home
 
-import androidx.compose.foundation.layout.Box
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import dev.glimmr.starwarssample.data.model.Starship
 import dev.glimmr.starwarssample.ui.theme.StarWarsSampleTheme
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    HomeScreenContent()
+    HomeScreenContent(
+        homeScreenState = viewModel.getUiState()
+    )
 }
 
 @Composable
-fun HomeScreenContent() {
-    Scaffold {
-        Box(modifier = Modifier.padding(it)){
-            Text(text = "Test")
+fun HomeScreenContent(
+    homeScreenState: State<HomeScreenState>
+) {
+    val uiState = homeScreenState.value
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxWidth(),
+//                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            when(uiState) {
+                is HomeScreenState.Loading -> Text(text = "loading")
+                is HomeScreenState.Ready -> StarshipList(
+                    starships = uiState.starships
+                )
+            }
         }
     }
 }
 
+@Composable
+fun StarshipList(
+    starships: List<Starship>,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn (
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(all = 16.dp),
+        modifier = modifier
+    ){
+        items(starships) {starship ->
+            StarshipCard(ship = starship)
+        }
+    }
+}
+
+@Composable
+fun StarshipCard(
+    ship: Starship,
+    modifier: Modifier =  Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(text = ship.name)
+        Text(text = ship.model)
+        Text(text = ship.starshipClass)
+        Text(text = ship.manufacturer)
+    }
+}
+
 //region Previews
+@SuppressLint("UnrememberedMutableState")
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreenContentPreview() {
     StarWarsSampleTheme {
-        HomeScreenContent()
+        HomeScreenContent(
+            homeScreenState = mutableStateOf(
+                HomeScreenState.Ready(
+                    starships = emptyList()
+                )
+            )
+        )
+    }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomeScreenContentPreviewLoading() {
+    StarWarsSampleTheme {
+        HomeScreenContent(
+            homeScreenState = mutableStateOf(HomeScreenState.Loading)
+        )
     }
 }
 //endregion
--- app/src/main/java/dev/glimmr/starwarssample/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/dev/glimmr/starwarssample/ui/screen/home/HomeViewModel.kt
@@ -1,6 +1,44 @@
 package dev.glimmr.starwarssample.ui.screen.home
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.glimmr.starwarssample.data.model.Starship
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeViewModel: ViewModel() {
+    private var uiState: MutableState<HomeScreenState> = mutableStateOf(HomeScreenState.Loading)
+
+    fun getUiState(): State<HomeScreenState>{
+        viewModelScope.launch {
+            delay(5000)
+            val newState = HomeScreenState.Ready(
+                List(size = 100){
+                    (
+                        Starship(
+                            "TestName $it",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                        )
+                    )
+                }
+            )
+            uiState.value = newState
+        }
+
+        return uiState
+    }
+}
+
+sealed class HomeScreenState {
+    object Loading: HomeScreenState()
+    class Ready(
+        val starships: List<Starship>
+    ): HomeScreenState()
 }
